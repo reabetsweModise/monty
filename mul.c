0966f6405ca43cff72ca9ccabcce9cb76908bb6d
reabetsweModise <reabetswemodise02@gmail.com>
2023-08-24 17:34:03 -0700

    renaming of files

---
+++ mul.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * f_mul - multiplies the first two elements in the stack.
+ * @head: head of the stack
+ * @counter: number of lines
+*/
+void f_mul(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int line_counter = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		line_counter++;
+	}
+	if (line_counter < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->next->n * h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- does nothing but point
+  *@head: head of the stack
+  *@counter: number of lines
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
+
